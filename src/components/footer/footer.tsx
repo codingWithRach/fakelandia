b4616a1328ab1880dea7cf67b2524e8eb3c1e35f
Rachel Miles <rachmiles@gmail.com>
2022-04-28 14:57:12 +0100

    Add spacing to footer

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -3,6 +3,7 @@ import "./footer.css";
 const Footer: React.FC = () => {
   return (
     <>
+      <div className="footer__spacing" />
       <footer className="footer">This footer is displayed on all pages</footer>
     </>
   );
