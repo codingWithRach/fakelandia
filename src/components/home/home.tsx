428e1fa2342490cd5b696c2bedc46263a3886d5d
Rachel Miles <rachmiles@gmail.com>
2022-04-28 14:56:50 +0100

    Add text to home page

--- src/components/home/home.tsx
+++ src/components/home/home.tsx
@@ -1,7 +1,12 @@
 const Home: React.FC = () => {
   return (
     <>
-      <h1>Home</h1>
+      <h1>Fakelandia Justice Department</h1>
+      <div>Welcome to the home of the Justice Department of Fakelandia</div>
+      <div>
+        Here you can browse a list of recent misdemeanours committed by our
+        citizens, or you can confess to your own misdemeanour
+      </div>
     </>
   );
 };
