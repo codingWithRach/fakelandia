d8411755c333f5cf39189f92604344e4929a404a
Rachel Miles <rachmiles@gmail.com>
2022-04-28 09:06:50 +0100

    Create type for misdemeanours

---
+++ src/data/misdemeanours.ts
@@ -0,0 +1,7 @@
+const misdemeanours = [
+  "Mild Public Rudeness",
+  "Speaking in a Lift",
+  "Not Eating Your Vegetables",
+  "Supporting Manchester United",
+];
+export type Misdemeanour = typeof misdemeanours[number];
