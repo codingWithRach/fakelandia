137ed1070b2e2e1ab898a91c089ffd87a778d4d9
Rachel Miles <rachmiles@gmail.com>
2022-04-28 14:44:16 +0100

    Add footer

---
+++ src/components/footer/footer.tsx
@@ -0,0 +1,11 @@
+import "./footer.css";
+
+const Footer: React.FC = () => {
+  return (
+    <>
+      <footer className="footer">This footer is displayed on all pages</footer>
+    </>
+  );
+};
+
+export default Footer;
--- src/components/main_layout/main_layout.tsx
+++ src/components/main_layout/main_layout.tsx
@@ -1,4 +1,5 @@
 import { Outlet } from "react-router-dom";
+import Footer from "../footer/footer";
 import Header from "../header/header";
 
 const MainLayout: React.FC = () => {
@@ -6,6 +7,7 @@ const MainLayout: React.FC = () => {
     <>
       <Header />
       <Outlet />
+      <Footer />
     </>
   );
 };
