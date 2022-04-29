5c5e68c1b087c7205953897ab82bf6a73ab99af5
Rachel Miles <rachmiles@gmail.com>
2022-04-29 08:48:33 +0100

    Add style class for page text

--- src/components/home/home.tsx
+++ src/components/home/home.tsx
@@ -1,9 +1,11 @@
 const Home: React.FC = () => {
   return (
     <>
-      <h1>Fakelandia Justice Department</h1>
-      <div>Welcome to the home of the Justice Department of Fakelandia</div>
-      <div>
+      <h1 className="pageHeading">Fakelandia Justice Department</h1>
+      <div className="pageText">
+        Welcome to the home of the Justice Department of Fakelandia
+      </div>
+      <div className="pageText">
         Here you can browse a list of recent misdemeanours committed by our
         citizens, or you can confess to your own misdemeanour
       </div>
