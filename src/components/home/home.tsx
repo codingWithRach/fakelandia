953ba268980075240b06caa7b0afec45696eae9e
Rachel Miles <rachmiles@gmail.com>
2022-04-29 14:10:39 +0100

    Add statistics to home page

--- src/components/home/home.tsx
+++ src/components/home/home.tsx
@@ -1,4 +1,8 @@
+import { useMisdemeanours } from "../../context/MisdemeanoursContext";
+
 const Home: React.FC = () => {
+  const misdemeanours = useMisdemeanours();
+
   return (
     <>
       <h1 className="pageHeading">Fakelandia Justice Department</h1>
@@ -8,6 +12,10 @@ const Home: React.FC = () => {
       <div className="pageText">
         Here you can browse a list of recent misdemeanours committed by our
         citizens, or you can confess to your own misdemeanour
+      </div>
+      <h2 className="subHeading">Statistics</h2>
+      <div className="pageText">
+        Number of misdemeanours currently reported: {misdemeanours.length}
       </div>
     </>
   );
